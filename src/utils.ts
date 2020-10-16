cb559e86d8596cc27ff043d3aca7588279da47ac
Tyler Kindy <tylerkindy@gmail.com>
2020-10-16 19:54:28 -0400

    Fix partition function

--- src/utils.ts
+++ src/utils.ts
@@ -3,7 +3,10 @@ export const partition: <T>(ts: T[], size: number) => T[][] = <T>(
   size: number
 ) => {
   return ts.reduce((acc, t, i) => {
-    const curPartition = i % size === 0 ? [] : acc[acc.length - 1];
-    return acc.slice(0, acc.length - 1).concat([curPartition.concat([t])]);
+    const [curPartition, donePartitions] =
+      i % size === 0
+        ? [[], acc]
+        : [acc[acc.length - 1], acc.slice(0, acc.length - 1)];
+    return donePartitions.concat([curPartition.concat([t])]);
   }, [] as T[][]);
 };
