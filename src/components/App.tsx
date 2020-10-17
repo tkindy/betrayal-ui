618f40940de7ebf54a91188fecf4703c18c31498
Tyler Kindy <tylerkindy@gmail.com>
2020-10-17 00:12:18 -0400

    Add barebones controls component

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -4,6 +4,7 @@ import { Layer, Stage } from 'react-konva';
 import { Direction } from './board/Room';
 import Board from './board/Board';
 import { PlayerColor } from './board/Players';
+import Controls from './controls/Controls';
 
 const getWindowDimensions = () => {
   const { innerWidth: width, innerHeight: height } = window;
@@ -31,49 +32,52 @@ const App = () => {
   const { width, height } = useWindowDimensions();
 
   return (
-    <Stage width={width} height={height} draggable>
-      <Layer>
-        <Board
-          rooms={[
-            {
-              name: 'Bloody Room',
-              loc: { gridX: 2, gridY: 2 },
-              doorDirections: [
-                Direction.SOUTH,
-                Direction.EAST,
-                Direction.NORTH,
-                Direction.WEST,
-              ],
-              players: [],
-            },
-            {
-              name: 'Statuary Corridor',
-              loc: { gridX: 1, gridY: 2 },
-              doorDirections: [Direction.EAST, Direction.SOUTH],
-              players: [{ color: PlayerColor.BLUE }],
-            },
-            {
-              name: 'Master Bedroom',
-              loc: { gridX: 2, gridY: 3 },
-              doorDirections: [Direction.NORTH, Direction.SOUTH],
-              players: [],
-            },
-            {
-              name: 'Crypt',
-              loc: { gridX: 2, gridY: 1 },
-              doorDirections: [Direction.SOUTH],
-              players: [
-                { color: PlayerColor.YELLOW },
-                { color: PlayerColor.RED },
-                { color: PlayerColor.GREEN },
-                { color: PlayerColor.WHITE },
-                { color: PlayerColor.PURPLE },
-              ],
-            },
-          ]}
-        />
-      </Layer>
-    </Stage>
+    <div>
+      <Controls />
+      <Stage width={width} height={height} draggable>
+        <Layer>
+          <Board
+            rooms={[
+              {
+                name: 'Bloody Room',
+                loc: { gridX: 2, gridY: 2 },
+                doorDirections: [
+                  Direction.SOUTH,
+                  Direction.EAST,
+                  Direction.NORTH,
+                  Direction.WEST,
+                ],
+                players: [],
+              },
+              {
+                name: 'Statuary Corridor',
+                loc: { gridX: 1, gridY: 2 },
+                doorDirections: [Direction.EAST, Direction.SOUTH],
+                players: [{ color: PlayerColor.BLUE }],
+              },
+              {
+                name: 'Master Bedroom',
+                loc: { gridX: 2, gridY: 3 },
+                doorDirections: [Direction.NORTH, Direction.SOUTH],
+                players: [],
+              },
+              {
+                name: 'Crypt',
+                loc: { gridX: 2, gridY: 1 },
+                doorDirections: [Direction.SOUTH],
+                players: [
+                  { color: PlayerColor.YELLOW },
+                  { color: PlayerColor.RED },
+                  { color: PlayerColor.GREEN },
+                  { color: PlayerColor.WHITE },
+                  { color: PlayerColor.PURPLE },
+                ],
+              },
+            ]}
+          />
+        </Layer>
+      </Stage>
+    </div>
   );
 };
 
---
+++ src/components/controls/Controls.tsx
@@ -0,0 +1,9 @@
+import React, { FunctionComponent } from 'react';
+
+interface ControlsProps {}
+
+const Controls: FunctionComponent<ControlsProps> = () => {
+  return null;
+};
+
+export default Controls;
