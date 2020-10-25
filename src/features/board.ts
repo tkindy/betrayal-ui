d9d53951e0d9d47225928d85ef1909789fb04e9a
Tyler Kindy <tylerkindy@gmail.com>
2020-10-25 11:54:40 -0400

    Check incoming doors on room placement

--- src/features/board.ts
+++ src/features/board.ts
@@ -8,6 +8,19 @@ import { GridLoc } from '../components/board/grid';
 import { Direction } from '../components/room/Room';
 import { RootState } from '../rootReducer';
 
+const getMatchingDoor: (dir: Direction) => Direction = (dir) => {
+  switch (dir) {
+    case Direction.NORTH:
+      return Direction.SOUTH;
+    case Direction.SOUTH:
+      return Direction.NORTH;
+    case Direction.EAST:
+      return Direction.WEST;
+    case Direction.WEST:
+      return Direction.EAST;
+  }
+};
+
 interface PlaceRoomPayload {
   loc: GridLoc;
 }
@@ -17,12 +30,20 @@ export const placeRoom = createAction<PlaceRoomPayload>('board/placeRoom');
 export const openSpotClicked: (
   loc: GridLoc,
   from: Direction[]
-) => ThunkAction<void, RootState, unknown, Action<string>> = (loc) => (
+) => ThunkAction<void, RootState, unknown, Action<string>> = (loc, from) => (
   dispatch,
   getState
 ) => {
   const flippedRoom = getState().roomStack.flippedRoom;
   if (!flippedRoom) {
+    return;
+  }
+
+  const hasMatchingDoor = flippedRoom.doorDirections
+    .map(getMatchingDoor)
+    .some((dir) => from.includes(dir));
+
+  if (!hasMatchingDoor) {
     return;
   }
 
