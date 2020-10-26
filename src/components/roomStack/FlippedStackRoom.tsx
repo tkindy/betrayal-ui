d158df66885e26e335a85ea3269f03c91946aafc
Tyler Kindy <tylerkindy@gmail.com>
2020-10-26 08:00:10 -0400

    Implement crude snapping-back

--- src/components/roomStack/FlippedStackRoom.tsx
+++ src/components/roomStack/FlippedStackRoom.tsx
@@ -1,8 +1,9 @@
-import React, { FunctionComponent } from 'react';
+import React, { FunctionComponent, useState } from 'react';
 import { Group } from 'react-konva';
 import { useDispatch, useSelector } from 'react-redux';
 import { RootState } from '../../rootReducer';
-import { droppedInGrid, useGridSize } from '../board/grid';
+import { droppedOnGrid, useGridSize } from '../board/grid';
+import { Point } from '../geometry';
 import { BoundingBox } from '../layout';
 import Room, { Direction } from '../room/Room';
 import RoomName from '../room/RoomName';
@@ -23,6 +24,7 @@ const FlippedStackRoom: FunctionComponent<FlippedStackRoomProps> = ({
   const dispatch = useDispatch();
   const gridSize = useGridSize();
   const boardTopLeft = useSelector((state: RootState) => state.board.topLeft);
+  const [groupTopLeft, setGroupTopLeft] = useState<Point>({ x: 0, y: 0 });
 
   return (
     <Group
@@ -30,7 +32,11 @@ const FlippedStackRoom: FunctionComponent<FlippedStackRoomProps> = ({
       onDragEnd={(e) => {
         const droppedOn = droppedOnGrid(e, roomBox, gridSize, boardTopLeft);
         console.log(`dropped on (${droppedOn.gridX}, ${droppedOn.gridY})`);
+        setGroupTopLeft({ x: e.target.x(), y: e.target.y() });
+        setGroupTopLeft({ x: 0, y: 0 });
       }}
+      x={groupTopLeft.x}
+      y={groupTopLeft.y}
     >
       <Room box={roomBox} doorDirections={doorDirections} />
       <RoomName box={roomBox} name={name} />
