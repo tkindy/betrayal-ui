ac55fca18aa32e8594565f1feafe8136e6e11703
Tyler Kindy <tylerkindy@gmail.com>
2020-11-12 10:15:08 -0500

    Remove HOC

--- src/components/Hovercard.tsx
+++ src/components/Hovercard.tsx
@@ -1,20 +1,5 @@
-import { KonvaEventObject } from 'konva/types/Node';
-import React, { ComponentType, FunctionComponent, useState } from 'react';
-import { Group, Rect } from 'react-konva';
-import { Subtract } from 'utility-types';
-import { Point } from './geometry';
-
-type MouseEventHandler = (e: KonvaEventObject<MouseEvent>) => void;
-
-export interface HovercardMouseHandlers {
-  onMouseEnter: MouseEventHandler;
-  onMouseLeave: MouseEventHandler;
-  onMouseMove: MouseEventHandler;
-}
-
-export interface HasHovercardMouseHandlers {
-  hovercardMouseHandlers: HovercardMouseHandlers;
-}
+import React, { FunctionComponent } from 'react';
+import { Rect } from 'react-konva';
 
 interface HovercardProps {
   x: number;
@@ -26,26 +11,3 @@ const Hovercard: FunctionComponent<HovercardProps> = ({ x, y }) => {
 };
 
 export default Hovercard;
-
-export const withHovercard = <P extends HasHovercardMouseHandlers>(
-  Component: ComponentType<P>
-) => {
-  return (props: Subtract<P, HasHovercardMouseHandlers>) => {
-    const [visible, setVisible] = useState(false);
-    const [{ x, y }, setPoint] = useState<Point>({ x: 0, y: 0 });
-
-    return (
-      <Group>
-        <Component
-          {...(props as P)}
-          hovercardMouseHandlers={{
-            onMouseEnter: () => setVisible(true),
-            onMouseLeave: () => setVisible(false),
-            onMouseMove: (e) => setPoint({ x: e.evt.x + 10, y: e.evt.y + 10 }),
-          }}
-        />
-        {visible && <Hovercard x={x} y={y} />}
-      </Group>
-    );
-  };
-};
