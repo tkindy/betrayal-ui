31b6ba34119835e25f9fd22b5db0ded91434ebf0
Tyler Kindy <tylerkindy@gmail.com>
2020-11-12 10:19:17 -0500

    Use portal

--- src/components/Hovercard.tsx
+++ src/components/Hovercard.tsx
@@ -1,13 +1,19 @@
 import React, { FunctionComponent } from 'react';
 import { Rect } from 'react-konva';
+import OverlayPortal from './portal/OverlayPortal';
 
 interface HovercardProps {
+  enabled: boolean;
   x: number;
   y: number;
 }
 
-const Hovercard: FunctionComponent<HovercardProps> = ({ x, y }) => {
-  return <Rect x={x} y={y} width={100} height={30} fill="yellow" />;
+const Hovercard: FunctionComponent<HovercardProps> = ({ enabled, x, y }) => {
+  return (
+    <OverlayPortal enabled={enabled}>
+      <Rect x={x} y={y} width={100} height={30} fill="yellow" />
+    </OverlayPortal>
+  );
 };
 
 export default Hovercard;
