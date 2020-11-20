b8a0391f9568159fc8d7f6cab8b0290f87dae5a5
Tyler Kindy <tylerkindy@gmail.com>
2020-11-19 22:18:54 -0500

    Add home styling

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,10 +1,24 @@
+import './Home.css';
 import { RouteComponentProps } from '@reach/router';
 import React, { FC } from 'react';
+import { useDispatch } from 'react-redux';
 
 interface HomeProps extends RouteComponentProps {}
 
 const Home: FC<HomeProps> = () => {
-  return <h1>Home</h1>;
+  const dispatch = useDispatch();
+
+  return (
+    <div>
+      <h1>Betrayal at House on the Hill</h1>
+      <div className="container">
+        <button className="new-game">New game</button>
+
+        <input className="join-game" />
+        <button className="join-game">Join game</button>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
