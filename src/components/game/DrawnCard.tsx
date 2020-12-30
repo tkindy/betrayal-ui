f5d344fd78c55b5ae1d6b12590163d3c2952bda7
Tyler Kindy <tylerkindy@gmail.com>
2020-12-30 13:43:45 -0500

    Implement crude event card rendering

--- src/components/game/DrawnCard.tsx
+++ src/components/game/DrawnCard.tsx
@@ -1,26 +1,140 @@
-import React, { FunctionComponent, ReactElement } from 'react';
+import React, { FunctionComponent } from 'react';
 import { Group, Rect, Text } from 'react-konva';
 import { useSelector } from 'react-redux';
-import { EventCard, ItemCard, OmenCard } from '../../features/models';
+import {
+  EventCard as EventCardModel,
+  ItemCard as ItemCardModel,
+  OmenCard as OmenCardModel,
+  RollTable,
+  RollTableRow,
+} from '../../features/models';
 import { RootState } from '../../store';
+import { BoundingBox } from '../layout';
 import { useWindowDimensions } from '../windowDimensions';
+import FlexContainer, { FlexDirection } from './sidebar/flex/FlexContainer';
 
-const paintEvent: (card: EventCard) => ReactElement<any, any> | null = (
-  card
-) => {
+interface BaseCardProps {
+  box: BoundingBox;
+  color: string;
+}
+
+const BaseCard: FunctionComponent<BaseCardProps> = ({
+  box: {
+    topLeft: { x, y },
+    dimensions: { width, height },
+  },
+  color,
+}) => {
+  return (
+    <Rect
+      x={x}
+      y={y}
+      width={width}
+      height={height}
+      cornerRadius={10}
+      fill={color}
+      stroke="black"
+    />
+  );
+};
+
+interface EventCardProps {
+  box: BoundingBox;
+  card: EventCardModel;
+}
+
+const renderRollTableRow = (row: RollTableRow) => {
+  switch (row.target.type) {
+    case 'EXACT':
+      return `${row.target.target}\t${row.outcome}`;
+    case 'RANGE':
+      return `${row.target.start}-${row.target.end}\t${row.outcome}`;
+    case 'MIN':
+      return `${row.target.minimum}+\t${row.outcome}`;
+  }
+};
+
+const renderRollTable = (rollTable: RollTable | undefined) => {
+  return rollTable?.reduce((acc, row) => {
+    return acc + '\n\n' + renderRollTableRow(row);
+  }, '');
+};
+
+const EventCard: FunctionComponent<EventCardProps> = ({ box, card }) => {
   return (
     <Group>
-      <Rect width={100} height={75} fill="white" />
-      <Text text={card.name} />
+      <BaseCard box={box} color="#a5c96c" />
+      <FlexContainer debug={true} box={box} direction={FlexDirection.COLUMN}>
+        {[
+          {
+            units: 2,
+            render: ({ topLeft: { x, y }, dimensions: { width, height } }) => (
+              <Text
+                x={x}
+                y={y}
+                width={width}
+                height={height}
+                text={card.name}
+                fontSize={20}
+                fontStyle="bold"
+                align="center"
+                verticalAlign="middle"
+              />
+            ),
+          },
+          {
+            units: 3,
+            render: ({ topLeft: { x, y }, dimensions: { width, height } }) => (
+              <Text
+                x={x}
+                y={y}
+                width={width}
+                height={height}
+                text={card.flavorText?.replace(/\n/g, '\n\n')}
+                fontSize={16}
+                fontStyle="italic"
+                align="center"
+                verticalAlign="middle"
+              />
+            ),
+          },
+          {
+            units: 15,
+            render: ({ topLeft: { x, y }, dimensions: { width, height } }) => (
+              <Text
+                x={x}
+                y={y}
+                width={width}
+                height={height}
+                text={card.description
+                  .replace(/\n/g, '\n\n')
+                  .replace(
+                    '<rollTable>',
+                    renderRollTable(card.rollTable) || 'MISSING ROW TABLE'
+                  )}
+                fontSize={16}
+              />
+            ),
+          },
+        ]}
+      </FlexContainer>
     </Group>
   );
 };
 
-const paintItem: (card: ItemCard) => ReactElement<any, any> | null = (card) => {
+interface ItemCardProps {
+  card: ItemCardModel;
+}
+
+const ItemCard: FunctionComponent<ItemCardProps> = ({ card }) => {
   return null;
 };
 
-const paintOmen: (card: OmenCard) => ReactElement<any, any> | null = (card) => {
+interface OmenCardProps {
+  card: OmenCardModel;
+}
+
+const OmenCard: FunctionComponent<OmenCardProps> = ({ card }) => {
   return null;
 };
 
@@ -36,23 +150,33 @@ const DrawnCard: FunctionComponent<DrawnCardProps> = () => {
     return null;
   }
 
-  let paintedCard: ReactElement<any, any> | null;
+  const cardHeight = height * 0.75;
+  const cardWidth = cardHeight / 2;
+  const cardBox: BoundingBox = {
+    topLeft: {
+      x: width / 2 - cardWidth / 2,
+      y: height / 2 - cardHeight / 2,
+    },
+    dimensions: { width: cardWidth, height: cardHeight },
+  };
+
+  let cardElement;
   switch (drawnCard.type) {
     case 'EVENT':
-      paintedCard = paintEvent(drawnCard);
+      cardElement = <EventCard box={cardBox} card={drawnCard} />;
       break;
     case 'ITEM':
-      paintedCard = paintItem(drawnCard);
+      cardElement = <ItemCard card={drawnCard} />;
       break;
     case 'OMEN':
-      paintedCard = paintOmen(drawnCard);
+      cardElement = <OmenCard card={drawnCard} />;
       break;
   }
 
   return (
     <Group>
       <Rect width={width} height={height} fill="gray" opacity={0.8} />
-      {paintedCard}
+      {cardElement}
     </Group>
   );
 };
