9a3a2f3cdb99a93b7c7c8be94c5a188ab62fefa4
Tyler Kindy <tylerkindy@gmail.com>
2020-12-30 11:46:56 -0500

    Implement drawing actions

--- src/features/cardStacks.ts
+++ src/features/cardStacks.ts
@@ -1,6 +1,33 @@
 import { createSlice } from '@reduxjs/toolkit';
+import * as api from '../api/api';
+import { Card } from './models';
+import { getGameId } from './selectors';
+import { createAsyncThunk } from './utils';
 
-interface CardStacksState {}
+export const drawEvent = createAsyncThunk(
+  'cardStacks/events/get',
+  async (_, { getState }) => {
+    return api.drawEvent(getGameId(getState()));
+  }
+);
+
+export const drawItem = createAsyncThunk(
+  'cardStacks/items/get',
+  async (_, { getState }) => {
+    return api.drawItem(getGameId(getState()));
+  }
+);
+
+export const drawOmen = createAsyncThunk(
+  'cardStacks/omens/get',
+  async (_, { getState }) => {
+    return api.drawOmen(getGameId(getState()));
+  }
+);
+
+interface CardStacksState {
+  drawnCard?: Card;
+}
 
 const initialState: CardStacksState = {};
 
@@ -8,6 +35,18 @@ const cardStacksSlice = createSlice({
   name: 'cardStacks',
   initialState,
   reducers: {},
+  extraReducers: (builder) => {
+    builder
+      .addCase(drawEvent.fulfilled, (state, { payload: card }) => {
+        state.drawnCard = card;
+      })
+      .addCase(drawItem.fulfilled, (state, { payload: card }) => {
+        state.drawnCard = card;
+      })
+      .addCase(drawOmen.fulfilled, (state, { payload: card }) => {
+        state.drawnCard = card;
+      });
+  },
 });
 
 export default cardStacksSlice.reducer;
