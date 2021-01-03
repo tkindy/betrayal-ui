fc9b2837f5124b46e4736fcb0ef22eae866c6739
Tyler Kindy <tylerkindy@gmail.com>
2021-01-03 09:25:39 -0500

    Add room description hovercards

--- src/components/game/board/BoardRoom.tsx
+++ src/components/game/board/BoardRoom.tsx
@@ -1,19 +1,67 @@
-import React, { FunctionComponent } from 'react';
-import { Group } from 'react-konva';
+import React, { FunctionComponent, useState } from 'react';
+import { Group, Text } from 'react-konva';
 import Room from '../room/Room';
 import RoomName from '../room/RoomName';
 import { useGridBox } from './grid';
 import { Room as RoomModel } from '../../../features/models';
-import { getRoomDetailsBox } from '../../layout';
+import { BoundingBox, Dimensions, getRoomDetailsBox } from '../../layout';
 import RoomFeatures from '../room/RoomFeatures';
 import { translate } from '../../geometry';
+import Hovercard, { CardDirection } from '../../Hovercard';
 
-const BoardRoom: FunctionComponent<RoomModel> = ({
-  name,
-  loc,
-  doorDirections,
-  features,
+const hovercardDimensions: Dimensions = { width: 200, height: 200 };
+
+interface RoomHovercardProps {
+  hovered: boolean;
+  roomBox: BoundingBox;
+  room: RoomModel;
+}
+
+const RoomHovercard: FunctionComponent<RoomHovercardProps> = ({
+  hovered,
+  roomBox,
+  room: { name, description },
 }) => {
+  return (
+    <Hovercard
+      enabled={hovered}
+      targetBox={roomBox}
+      contentDimensions={hovercardDimensions}
+      direction={CardDirection.RIGHT}
+      renderContent={({ topLeft, dimensions: { width, height } }) => {
+        const { x, y } = topLeft;
+        return (
+          <Group x={x} y={y}>
+            <Text
+              x={0}
+              y={0}
+              width={width}
+              height={height / 10}
+              text={name}
+              align="center"
+              verticalAlign="middle"
+              fontSize={20}
+              fontStyle="bold"
+            />
+            <Text
+              x={0}
+              y={(2 * height) / 10}
+              width={width}
+              height={(8 * height) / 10}
+              text={description}
+              align="center"
+              verticalAlign="middle"
+              fontSize={16}
+            />
+          </Group>
+        );
+      }}
+    />
+  );
+};
+
+const BoardRoom: FunctionComponent<RoomModel> = (room) => {
+  const { name, loc, doorDirections, features } = room;
   const box = useGridBox(loc);
   const detailsBox = getRoomDetailsBox(box);
   const {
@@ -28,12 +76,20 @@ const BoardRoom: FunctionComponent<RoomModel> = ({
     topLeft: translate(detailsTopLeft, 0, detailsHeight / 2),
     dimensions: { width: detailsWidth, height: detailsHeight / 2 },
   };
+  const [hovered, setHovered] = useState(false);
 
   return (
     <Group>
-      <Room box={box} doorDirections={doorDirections} />
-      <RoomName box={nameBox} name={name} />
-      <RoomFeatures box={featuresBox} features={features} />
+      <Group
+        onTap={() => setHovered(!hovered)}
+        onMouseEnter={() => setHovered(true)}
+        onMouseLeave={() => setHovered(false)}
+      >
+        <Room box={box} doorDirections={doorDirections} />
+        <RoomName box={nameBox} name={name} />
+        <RoomFeatures box={featuresBox} features={features} />
+      </Group>
+      <RoomHovercard hovered={hovered} roomBox={box} room={room} />
     </Group>
   );
 };
