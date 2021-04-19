8f291bd596de4ff488444b9ab811fd84d8efe7e3
Tyler Kindy <tylerkindy@gmail.com>
2021-04-19 17:36:17 -0400

    Use dice images (#45)

--- src/components/game/dice/Dice.tsx
+++ src/components/game/dice/Dice.tsx
@@ -1,26 +1,14 @@
 import React, { FC, useState } from 'react';
 import DiceButton from './DiceButton';
-import DiceRoller from './DiceRoller';
+import DiceControl from './DiceControl';
+import './Dice.css';
 
 const Dice: FC<{}> = () => {
   const [expanded, setExpanded] = useState(false);
   return (
-    <div>
-      <DiceButton
-        box={{
-          topLeft: { x: 10, y: 10 },
-          dimensions: { width: 50, height: 50 },
-        }}
-        expanded={expanded}
-        onClick={() => setExpanded(!expanded)}
-      />
-      <DiceRoller
-        box={{
-          topLeft: { x: 10, y: 70 },
-          dimensions: { width: 150, height: 150 },
-        }}
-        expanded={expanded}
-      />
+    <div className="dice-container">
+      <DiceButton expanded={expanded} onClick={() => setExpanded(!expanded)} />
+      <DiceControl expanded={expanded} />
     </div>
   );
 };
---
+++ src/components/game/dice/DiceControl.tsx
@@ -0,0 +1,102 @@
+import React, { FunctionComponent, useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { rollDice } from '../../../features/diceRolls';
+import { RootState } from '../../../store';
+import zero from './zero.svg';
+import one from './one.svg';
+import two from './two.svg';
+import './DiceControl.css';
+
+interface DieProps {
+  value: number;
+}
+
+const Die: FunctionComponent<DieProps> = ({ value }) => {
+  let src;
+  let alt;
+  switch (value) {
+    case 0:
+      src = zero;
+      alt = 'zero';
+      break;
+    case 1:
+      src = one;
+      alt = 'one';
+      break;
+    case 2:
+      src = two;
+      alt = 'two';
+      break;
+  }
+
+  return <img src={src} alt={alt} className="die-value" />;
+};
+
+interface DiceRowProps {
+  values: number[];
+}
+
+const DiceRow: FunctionComponent<DiceRowProps> = ({ values }) => {
+  return (
+    <div className="dice-row">
+      {values.map((value) => (
+        <Die value={value} />
+      ))}
+    </div>
+  );
+};
+
+interface DiceProps {
+  values?: number[];
+}
+
+const Dice: FunctionComponent<DiceProps> = ({ values }) => {
+  let rows;
+  if (values) {
+    rows = [
+      <DiceRow values={values.slice(0, 4)} />,
+      <DiceRow values={values.slice(4, 8)} />,
+    ];
+  }
+
+  return <div>{rows}</div>;
+};
+
+interface RollDiceControlProps {
+  expanded: boolean;
+}
+
+const DiceControl: FunctionComponent<RollDiceControlProps> = ({ expanded }) => {
+  const roll = useSelector((state: RootState) => state.diceRolls.roll);
+  const dispatch = useDispatch();
+  const [numDice, setNumDice] = useState<number>(8);
+
+  if (!expanded) {
+    return null;
+  }
+
+  return (
+    <div className="dice-background">
+      <div className="dice-controls">
+        <input
+          className="dice-count"
+          type="number"
+          min="1"
+          max="8"
+          value={numDice}
+          onChange={(e) => setNumDice(parseInt(e.target.value))}
+        />
+        <button
+          className="roll-dice-button"
+          onClick={() => dispatch(rollDice({ numDice }))}
+        >
+          Roll
+        </button>
+      </div>
+
+      <Dice values={roll} />
+    </div>
+  );
+};
+
+export default DiceControl;
