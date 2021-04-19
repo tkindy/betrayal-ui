6c6996cb7affb0eb5a4b26110c56e2a7f0b73f47
Tyler Kindy <tylerkindy@gmail.com>
2021-04-19 08:40:33 -0400

    Add roller component

--- src/components/game/dice/Dice.tsx
+++ src/components/game/dice/Dice.tsx
@@ -1,14 +1,21 @@
 import React, { FC, useState } from 'react';
 import DiceButton from './DiceButton';
+import DiceRoller from './DiceRoller';
 
 const Dice: FC<{}> = () => {
   const [expanded, setExpanded] = useState(false);
   return (
-    <DiceButton
-      box={{ topLeft: { x: 10, y: 10 }, dimensions: { width: 60, height: 60 } }}
-      expanded={expanded}
-      onClick={() => setExpanded(!expanded)}
-    />
+    <div>
+      <DiceButton
+        box={{
+          topLeft: { x: 10, y: 10 },
+          dimensions: { width: 60, height: 60 },
+        }}
+        expanded={expanded}
+        onClick={() => setExpanded(!expanded)}
+      />
+      <DiceRoller />
+    </div>
   );
 };
 
---
+++ src/components/game/dice/DiceRoller.tsx
@@ -0,0 +1,7 @@
+import React, { FC } from 'react';
+
+const DiceRoller: FC<{}> = () => {
+  return null;
+};
+
+export default DiceRoller;
