c55eb60fd727e437db3624f3775f9bf3ce4a1395
Tyler Kindy <tylerkindy@gmail.com>
2021-04-19 08:19:38 -0400

    Use BoundingBox

--- src/components/game/dice/Dice.tsx
+++ src/components/game/dice/Dice.tsx
@@ -2,7 +2,11 @@ import React, { FC, useState } from 'react';
 import DiceButton from './DiceButton';
 
 const Dice: FC<{}> = () => {
-  return <DiceButton />;
+  return (
+    <DiceButton
+      box={{ topLeft: { x: 10, y: 10 }, dimensions: { width: 60, height: 60 } }}
+    />
+  );
 };
 
 export default Dice;
--- src/components/game/dice/DiceButton.tsx
+++ src/components/game/dice/DiceButton.tsx
@@ -1,24 +1,46 @@
 import React, { FC } from 'react';
 import die from '../../../assets/die.svg';
+import { BoundingBox } from '../../layout';
 
-const DiceButton: FC<{}> = () => {
+interface DiceButtonProps {
+  box: BoundingBox;
+}
+
+const DiceButton: FC<DiceButtonProps> = ({
+  box: {
+    topLeft: { x, y },
+    dimensions: { width, height },
+  },
+}) => {
+  const dieWidth = width / 2;
+  const dieHeight = dieWidth;
+  const dieLeft = width / 2 - dieWidth / 2;
+  const dieTop = height / 2 - dieHeight / 2;
+
   return (
     <div
       id="test-circle"
       style={{
         backgroundColor: '#cc3716',
         borderRadius: '50%',
-        margin: '10px',
-        width: '60px',
-        height: '60px',
+        position: 'absolute',
+        left: x,
+        top: y,
+        width,
+        height,
         cursor: 'pointer',
       }}
     >
       <img
         src={die}
-        style={{ position: 'relative', left: 15, top: 15, cursor: 'pointer' }}
-        width="30px"
-        height="30px"
+        style={{
+          position: 'relative',
+          left: dieLeft,
+          top: dieTop,
+          cursor: 'pointer',
+        }}
+        width={dieWidth}
+        height={dieHeight}
       />
     </div>
   );
