7ebe76afbe57fc24e6dce241b705ee39fc877ca4
Tyler Kindy <tylerkindy@gmail.com>
2021-05-11 23:10:37 -0400

    Finish-ish RoomStack

--- src/components/game/sidebar/roomStack/RoomStack.tsx
+++ src/components/game/sidebar/roomStack/RoomStack.tsx
@@ -1,25 +1,14 @@
 import React, { FunctionComponent } from 'react';
 import RoomStackControl from './RoomStackControl';
-import { translate } from '../../../geometry';
-import { BoundingBox } from '../../../layout';
-import FlexContainer, { FlexDirection } from '../flex/FlexContainer';
 import StackRoom from './StackRoom';
 
-interface RoomStackProps {
-  box: BoundingBox;
-}
+interface RoomStackProps {}
 
-const RoomStack: FunctionComponent<RoomStackProps> = ({ box }) => {
+const RoomStack: FunctionComponent<RoomStackProps> = () => {
   return (
     <div style={{ display: 'flex', flexDirection: 'column' }}>
       <StackRoom />
-      <FlexContainer box={box} direction={FlexDirection.COLUMN}>
-        {[
-          { units: 8, render: () => null },
-          { units: 1, render: () => null },
-          { units: 2, render: (box) => <RoomStackControl box={box} /> },
-        ]}
-      </FlexContainer>
+      <RoomStackControl />
     </div>
   );
 };
