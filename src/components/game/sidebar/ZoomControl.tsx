fa8431ef15b165bb7a37d10606b8b24c232cdc63
Tyler Kindy <tylerkindy@gmail.com>
2021-05-11 22:40:07 -0400

    Start unwrapping ZoomControl

--- src/components/game/sidebar/ZoomControl.tsx
+++ src/components/game/sidebar/ZoomControl.tsx
@@ -2,7 +2,6 @@ import React, { FunctionComponent } from 'react';
 import { useDispatch } from 'react-redux';
 import { zoomIn, zoomOut } from '../../../features/zoom';
 import { BoundingBox } from '../../layout';
-import DOMPortal from '../portal/DOMPortal';
 import './ZoomControl.css';
 
 interface ZoomControlProps {
@@ -11,40 +10,30 @@ interface ZoomControlProps {
 
 const ZoomControl: FunctionComponent<ZoomControlProps> = ({
   box: {
-    topLeft: { x, y },
-    dimensions: { width, height },
+    dimensions: { height },
   },
 }) => {
   const dispatch = useDispatch();
 
   return (
-    <DOMPortal name="zoomControl">
-      <div
-        style={{
-          position: 'absolute',
-          top: y,
-          left: x,
-          width,
-          height,
-          display: 'flex',
-          justifyContent: 'space-around',
-          alignItems: 'center',
-        }}
+    <div
+      style={{
+        height,
+        display: 'flex',
+        justifyContent: 'space-around',
+        alignItems: 'center',
+      }}
+    >
+      <button className="zoomButton zoomIn" onClick={() => dispatch(zoomIn())}>
+        +
+      </button>
+      <button
+        className="zoomButton zoomOut"
+        onClick={() => dispatch(zoomOut())}
       >
-        <button
-          className="zoomButton zoomIn"
-          onClick={() => dispatch(zoomIn())}
-        >
-          +
-        </button>
-        <button
-          className="zoomButton zoomOut"
-          onClick={() => dispatch(zoomOut())}
-        >
-          -
-        </button>
-      </div>
-    </DOMPortal>
+        -
+      </button>
+    </div>
   );
 };
 
