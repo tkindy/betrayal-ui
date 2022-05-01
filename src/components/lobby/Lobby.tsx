0165bb007a1eaf7e1dd6c6840f78b6db649a2bf0
Tyler Kindy <tylerkindy@gmail.com>
2022-04-30 22:58:59 -0400

    Fill out lobby component

--- src/components/lobby/Lobby.tsx
+++ src/components/lobby/Lobby.tsx
@@ -1,7 +1,55 @@
-import { FC } from 'react';
+import { connect, disconnect } from '@giantmachines/redux-websocket/dist';
+import { FC, useEffect } from 'react';
+import { useNavigate, useParams } from 'react-router-dom';
+import { joinLobby } from '../../features/lobby';
+import { LobbyPlayer } from '../../features/models';
+import { useAppDispatch, useAppSelector } from '../../hooks';
+
+const buildWebsocketUrl = (lobbyId: string) => {
+  const httpRoot = process.env.REACT_APP_API_ROOT!!;
+  const wsRoot = httpRoot.replace(/^http/, 'ws');
+  return `${wsRoot}/lobbies/${lobbyId}`;
+};
+
+const PlayerList: FC<{ players?: LobbyPlayer[] }> = ({ players }) => {
+  if (!players) {
+    return <p>Loading...</p>;
+  }
+
+  return (
+    <ul className="lobby-players">
+      {players.map((player) => (
+        <li key={player.id}>{player.name}</li>
+      ))}
+    </ul>
+  );
+};
 
 const Lobby: FC<{}> = () => {
-  return <p>Lobby!</p>;
+  const { lobbyId } = useParams();
+  const navigate = useNavigate();
+  const dispatch = useAppDispatch();
+  const players = useAppSelector((state) => state.lobby.players);
+
+  useEffect(() => {
+    if (!lobbyId || !/^[A-Z]{6}$/.test(lobbyId)) {
+      navigate('/');
+      return;
+    }
+
+    dispatch(joinLobby(lobbyId));
+    dispatch(connect(buildWebsocketUrl(lobbyId)));
+
+    return () => {
+      dispatch(disconnect());
+    };
+  }, [lobbyId, dispatch, navigate]);
+
+  return (
+    <div className="lobby-wrapper">
+      <PlayerList players={players} />
+    </div>
+  );
 };
 
 export default Lobby;
